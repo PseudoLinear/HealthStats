18372f421cfe9bf1fc787e0c44dbe8d4b7cfdeb5
trey devine <treycd2@gmail.com>
2018-08-22 13:27:45 -0400

    Aug 22

--- HealthStatsWeb/Controllers/UserController.cs
+++ HealthStatsWeb/Controllers/UserController.cs
@@ -49,7 +49,7 @@ namespace HealthStatsWeb.Controllers
                 else
                 {
                     //return register user
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("CreateUser", "Register");
                 }
 
 
@@ -91,7 +91,7 @@ namespace HealthStatsWeb.Controllers
         {
             if (Session["Role_ID"] != null)
             {
-                if ((int)Session["Role_ID"] == 3)
+                if ((int)Session["Role_ID"] == 2 || ((int)Session["Role_ID"] == 3))
                 {
                     //instantiate and name 
                     UserViewModel _viewModel = new UserViewModel();
@@ -134,17 +134,48 @@ namespace HealthStatsWeb.Controllers
         [HttpGet]
         public ActionResult UpdateUser(int User_ID)
         {
-            User UserToUpdate = _Mapper.Map(_UserDataAccess.GetUserByUser_ID(User_ID));
+            if (Session["Role_ID"] != null)
+            {
+                if ((int)Session["Role_ID"] == 3)
+                {
+                    User UserToUpdate = _Mapper.Map(_UserDataAccess.GetUserByUser_ID(User_ID));
+                    return View(UserToUpdate);
+                }
+                else
+                {
+                    return RedirectToAction("Login", "User");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Login", "User");
+            }
+            
 
-            return View(UserToUpdate);
+
+           
         }
         [HttpPost]
         public ActionResult UpdateUser(User _UserToUpdate)
         {
+            if (Session["Role_ID"] != null)
+            {
+                if ((int)Session["Role_ID"] == 3)
+                {
+                    _UserDataAccess.UpdateUser(_Mapper.Map(_UserToUpdate));
 
-            _UserDataAccess.UpdateUser(_Mapper.Map(_UserToUpdate));
-
-            return RedirectToAction("ViewUsers", "user");
+                    return RedirectToAction("ViewUsers", "user");
+                }
+                else
+                {
+                    return RedirectToAction("Login", "User");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Login", "User");
+            }
+            
         }
     }
 }
